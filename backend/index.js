cd73f9ee79f3a4a5a15fd59f405246e7603d835b
HumzaTAhmad <humza3173@gmail.com>
2022-09-29 10:44:04 -0400

    Import required libraries, delete front-end src folder, create server index file
    libraries imported to be used. front end folder contents were cleared
    for blank app.js and index.js files. server index file now connected to
    mongodb database

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb, extended: true"}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message))
