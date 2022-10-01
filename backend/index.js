1a2f01fd42b87085c7f08b3d8b1ae39160e2b8c8
HumzaTAhmad <humza3173@gmail.com>
2022-10-01 19:09:26 -0400

    Load data into accounts component from store using useSelector function
    This data that we load into accounts will be displayed onto the page,
    meaning every valorant account loaded onto the component will be neatly
    organized and shown to the user.

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,11 @@ dotenv.config()
 
 const app = express();
 
-app.use("/account", accountRoutes);
-
 app.use(bodyParser.json({limit: "30mb, extended: true"}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use("/account", accountRoutes);
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
--- frontend/src/components/Accounts/Accounts.js
+++ frontend/src/components/Accounts/Accounts.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 
 import Account from './Account/Account.js'
 import useStyles from './styles.js';
 
 const Accounts = () => {
+    const accounts = useSelector((state) => state.accounts)
     const classes = useStyles();
+
+    console.log(accounts);
 
     return (
         <>
