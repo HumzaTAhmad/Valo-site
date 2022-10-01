910b870cf74e7a3e6884325d0691edaec370ea33
HumzaTAhmad <humza3173@gmail.com>
2022-10-01 17:40:39 -0400

    Create Accounts, Account, and Form components
    created Form component so user can type the username and tag of the players
    valorant account, the Accounts component will display all of the players
    that are stored in the database on the screen. The Accounts component will
    get this information from our Account component which will be displaying
    one account at a time.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Accounts from './components/Accounts/Accounts.js';
+import Form from './components/Form/Form.js'
+import logo from './images/logo.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Valo-Site</Typography>
+                <img src={logo} alt="valo-site" height="60" width="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid Container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Accounts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
---
+++ frontend/src/components/Accounts/Accounts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Account from './Account/Account.js'
+
+const Accounts = () => {
+    return (
+        <>
+            <h1>Accounts</h1>
+            <Account />
+            <Account />
+        
+        </>
+    );
+}
+
+export default Accounts;
