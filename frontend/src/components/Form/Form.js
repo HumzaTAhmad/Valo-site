4976d1607c2243c9296f778c447384bae792e3c6
HumzaTAhmad <humza3173@gmail.com>
2022-10-06 16:33:42 -0400

    Fill out form component and create pleasant looking form
    Created a form that takes in the username and tag of the
    valorant player and on submit saves that information into
    accountData using the setAccountData Hook. This was done so
    we can pass this information to the backend to be searched up with
    the Riot API.

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper} from '@material-ui/core';
 import useStyles from './styles.js';
 
 const Form = () => {
 
     const classes = useStyles();
+    const [accountData, setAccountData] = useState({
+        username: '', 
+        tag: ''
+    });
+
+    const handleSubmit = () => {
+
+    }
+    const clear = () => {
+        
+    }
 
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant="h6">Search Valorant Account</Typography>
+            <TextField
+                name="username"
+                variant="outlined"
+                label="Username"
+                fullWidth
+                value={accountData.username}
+                onChange={(e) => setAccountData({ ...accountData, username: e.target.value})}
+            />
+             <TextField
+                name="tag"
+                variant="outlined"
+                label="Tag"
+                fullWidth
+                value={accountData.tag}
+                onChange={(e) => setAccountData({ ...accountData, tag: e.target.value})}
+            />
+            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+            <Button className={classes.buttonSubmit} variant="contained" color="secondary" size="small" oncClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     );
 }
 
