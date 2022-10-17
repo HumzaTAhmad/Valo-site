0f3d7de32d0d910f806f36b08caa0043c4ae1bc0
HumzaTAhmad <humza3173@gmail.com>
2022-10-17 15:01:45 -0400

    Import two custom logos, one for design and the other for delete logo. use rank image right after rank name
    Used the valorant to add design to the bottom left of the card. Replaced
    trash can delete logo for x mark to match with the background of the website
    Also added rank image based on the players rank

--- frontend/src/components/Accounts/Account/Account.js
+++ frontend/src/components/Accounts/Account/Account.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import {Card, CardActions, CardContent, CardMedia, Button, Typography, Icon} from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles.js';
-
+import val from '../../../images/val.svg'
+import delete_logo from '../../../images/delete_logo.svg'
 const Account = ({account}) => {
 
     const classes = useStyles();
@@ -24,19 +25,19 @@ const Account = ({account}) => {
             </div>
             <CardContent>
                 <Typography className={classes.title} variant="h6" gutterBottom>Username: {account.name}</Typography>
-                <Typography className={classes.title} variant="h6" gutterBottom>Tagline: {account.tag}</Typography>
-                <Typography className={classes.title} variant="h6" gutterBottom>Region: {account.region.toUpperCase()}</Typography>
+                <Typography className={classes.title} variant="h6" gutterBottom>Rank: {account.rank}<Icon>  <img src={account.rank_image} height={28} width={27}/></Icon></Typography>
                 <Typography className={classes.title} variant="h6" gutterBottom>Level: {account.account_level}</Typography>
-                <Typography className={classes.title} variant="h6" gutterBottom>Rank: {account.rank}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
-                    <ThumbUpAltIcon fontSize="small" />
-                    Like
+                <Button disabled="true" size="small" color="primary" onClick={() => {}}>
+                    <Icon fontSize='large'>
+                        <img src={val} height={40} width={20}/>
+                    </Icon>
                 </Button>
                 <Button size="small" color="primary" onClick={() => {}}>
-                    <DeleteIcon fontSize="small" />
-                    Delete
+                    <Icon fontSize='large'>
+                        <img src={delete_logo} height={40} width={20}/> Delete
+                    </Icon>
                 </Button>
             </CardActions>
         </Card>
